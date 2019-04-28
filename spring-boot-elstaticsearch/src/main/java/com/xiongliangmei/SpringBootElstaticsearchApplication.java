5c3894a57c69ced14891db9e0c64626930f97c9f
xiongliang <xiongliang@umtsoft.com>
2019-04-28 15:12:41 +0800

    spring-boot-shiro

---
+++ spring-boot-elstaticsearch/src/main/java/com/xiongliangmei/SpringBootElstaticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.xiongliangmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElstaticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElstaticsearchApplication.class, args);
+    }
+
+}
